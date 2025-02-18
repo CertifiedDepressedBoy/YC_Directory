8b728f77eb50e027624cdba8d3d76e6daf9556e2
Kaung <104748803+CertifiedDepressedBoy@users.noreply.github.com>
2025-02-18 16:50:44 +0630

    init

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-2xl">Home</h1>
+    <section className="pink_container">
+    <h1 className="heading">
+        Pitch your Startup, <br/>
+        Connect with Entrepreneurs
+    </h1>
+
+    <p className="sub-heading !max-w-3xl">
+        Submit ideas, Vote on Pitches , and Get Noticed iin Virtual Competitions.
+    </p>
+
+    <SearchForm />
+
+    </section>
     </>
   );
 }
