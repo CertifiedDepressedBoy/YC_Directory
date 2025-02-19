58bf8b23e94a138462aef6d9376cebc49d8de2ad
Kaung <104748803+CertifiedDepressedBoy@users.noreply.github.com>
2025-02-19 10:10:53 +0630

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
-import SearchForm from "../components/SearchForm";
+import SearchForm from "../../components/SearchForm";
+import * as querystring from "node:querystring";
+import {promises} from "node:dns";
 
-export default function Home() {
+    export default async function Home({searchParams} : {searchParams : Promise<{query?: string}>}){
+        const query = (await searchParams).query
   return (
     <>
     <section className="pink_container">
@@ -13,7 +16,7 @@ export default function Home() {
         Submit ideas, Vote on Pitches , and Get Noticed iin Virtual Competitions.
     </p>
 
-    <SearchForm />
+    <SearchForm query={query} />
 
     </section>
     </>
