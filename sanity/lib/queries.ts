9bf7290c87518d50350128f5a2c823c03ab9ef6d
Kaung <104748803+CertifiedDepressedBoy@users.noreply.github.com>
2025-02-21 11:02:37 +0630

    init

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -22,7 +22,7 @@ export const STARTUPS_BY_ID_QUERY =
         slug,
         _createdAt,
         author -> {
-        _id,name,image,bio
+        _id,name,image,bio,username
         },
         views,
         description,
@@ -30,3 +30,9 @@ export const STARTUPS_BY_ID_QUERY =
         image,
         pitch
     }`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+        *[_type == "startup" && _id == $id][0]{
+            _id , views
+        }
+    `)
